4d20e9271cdc77d2b8787fa0a505673355f887ae
Saúl Napoleón Haro Hernández <93763098+CuajoElRenacuajo@users.noreply.github.com>
2024-07-31 05:48:05 +0900

    eggplant

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//albahaca
 var sliderAlbahaca = document.getElementById("myRangeAlbahaca");
 var outputAlbahaca = document.getElementById("demoAlbahaca");
 outputAlbahaca.innerHTML = sliderAlbahaca.value;
@@ -52,7 +53,7 @@ outputBerenjenaBlanca.innerHTML = sliderBerenjenaBlanca.value;
 
 sliderBerenjenaBlanca.oninput = function() {
     outputBerenjenaBlanca.innerHTML = `Week: ${this.value}`;
-    document.getElementById("berenjenaBlanca").src=`assets/images/berenjenaBlanca/berenjenaBlanca${this.value}.jpg`;
+    document.getElementById("berenjenaBlanca").src=`assets/images/berenjenablanca/berenjenablanca${this.value}.jpg`;
     
         if(this.value == 1){
             document.getElementById("berenjenaBlancaD").innerHTML = `Cambios Importantes:
@@ -91,4 +92,104 @@ Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un in
             document.getElementById("berenjenaBlancaD").innerHTML = "Observation: 10";
         }
     }
+
+// berenjena
+
+var sliderBerenjena = document.getElementById("myRangeBerenjena");
+var outputBerenjena = document.getElementById("demoBerenjena");
+outputBerenjena.innerHTML = sliderBerenjena.value;
+
+sliderBerenjena.oninput = function() {
+    outputBerenjena.innerHTML = `Week: ${this.value}`;
+    document.getElementById("berenjena").src=`assets/images/berenjena/berenjena${this.value}.jpg`;
+    
+        if(this.value == 1){
+            document.getElementById("berenjenaD").innerHTML = `Cambios Importantes:
+Color Verde Vivo: La albahaca presenta un color verde vivo que indica buena salud y vitalidad.
+Frondosidad: La planta se muestra frondosa, con un crecimiento denso y exuberante.
+Modificaciones Observadas:
+Crecimiento de Hojas: Se observan nuevas hojas en desarrollo.
+Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un indicativo de que la planta está absorbiendo bien los nutrientes y el agua.
+`;
+        }
+        if(this.value == 2){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 2";
+        }
+        if(this.value == 3){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 3";
+        }
+        if(this.value == 4){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 4";
+        }
+        if(this.value == 5){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 5";
+        }
+        if(this.value == 6){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 6";
+        }
+        if(this.value == 7){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 7";
+        }
+        if(this.value == 8){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 8";
+        }
+        if(this.value == 9){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 9";
+        }
+        if(this.value == 10){
+            document.getElementById("berenjenaD").innerHTML = "Observation: 10";
+        }
+    }
+
+//chiles
+
+var sliderChiles = document.getElementById("myRangeChiles");
+var outputChiles = document.getElementById("demoChiles");
+outputChiles.innerHTML = sliderChiles.value;
+
+sliderChiles.oninput = function() {
+    outputChiles.innerHTML = `Week: ${this.value}`;
+    document.getElementById("chiles").src=`assets/images/chiles/chiles${this.value}.jpg`;
+    
+        if(this.value == 1){
+            document.getElementById("chilesD").innerHTML = `Cambios Importantes:
+Color Verde Vivo: La albahaca presenta un color verde vivo que indica buena salud y vitalidad.
+Frondosidad: La planta se muestra frondosa, con un crecimiento denso y exuberante.
+Modificaciones Observadas:
+Crecimiento de Hojas: Se observan nuevas hojas en desarrollo.
+Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un indicativo de que la planta está absorbiendo bien los nutrientes y el agua.
+`;
+        }
+        if(this.value == 2){
+            document.getElementById("chilesD").innerHTML = "Observation: 2";
+        }
+        if(this.value == 3){
+            document.getElementById("chilesD").innerHTML = "Observation: 3";
+        }
+        if(this.value == 4){
+            document.getElementById("chilesD").innerHTML = "Observation: 4";
+        }
+        if(this.value == 5){
+            document.getElementById("chilesD").innerHTML = "Observation: 5";
+        }
+        if(this.value == 6){
+            document.getElementById("chilesD").innerHTML = "Observation: 6";
+        }
+        if(this.value == 7){
+            document.getElementById("chilesD").innerHTML = "Observation: 7";
+        }
+        if(this.value == 8){
+            document.getElementById("chilesD").innerHTML = "Observation: 8";
+        }
+        if(this.value == 9){
+            document.getElementById("chilesD").innerHTML = "Observation: 9";
+        }
+        if(this.value == 10){
+            document.getElementById("chilesD").innerHTML = "Observation: 10";
+        }
+    }
+
+
+
+
 
