45254233039ae40ded859ad843610d5c72f6d8fb
Saúl Napoleón Haro Hernández <93763098+CuajoElRenacuajo@users.noreply.github.com>
2024-07-31 06:03:59 +0900

    hinojo

--- script.js
+++ script.js
@@ -189,6 +189,185 @@ Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un in
         }
     }
 
+//germanchamoline
+
+var sliderGermanChamoline = document.getElementById("myRangeGermanChamoline");
+var outputGermanChamoline = document.getElementById("demoGermanChamoline");
+outputGermanChamoline.innerHTML = sliderGermanChamoline.value;
+
+sliderGermanChamoline.oninput = function() {
+    outputGermanChamoline.innerHTML = `Week: ${this.value}`;
+    document.getElementById("germanchamoline").src=`assets/images/germanchamoline/germanchamoline${this.value}.jpg`;
+    
+ 
+        if(this.value == 3){
+            document.getElementById("germanchamolineD").innerHTML = `Cambios Importantes:
+Color Verde Vivo: La albahaca presenta un color verde vivo que indica buena salud y vitalidad.
+Frondosidad: La planta se muestra frondosa, con un crecimiento denso y exuberante.
+Modificaciones Observadas:
+Crecimiento de Hojas: Se observan nuevas hojas en desarrollo.
+Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un indicativo de que la planta está absorbiendo bien los nutrientes y el agua.
+`;
+            
+        }
+        if(this.value == 4){
+            document.getElementById("germanchamolineD").innerHTML = "Observation: 4";
+        }
+        if(this.value == 5){
+            document.getElementById("germanchamolineD").innerHTML = "Observation: 5";
+        }
+        if(this.value == 6){
+            document.getElementById("germanchamolineD").innerHTML = "Observation: 6";
+        }
+        if(this.value == 7){
+            document.getElementById("germanchamolineD").innerHTML = "Observation: 7";
+        }
+        
+    }
+
+//girasol
+
+var sliderGirasol = document.getElementById("myRangeGirasol");
+var outputGirasol = document.getElementById("demoGirasol");
+outputGirasol.innerHTML = sliderGirasol.value;
+
+sliderGirasol.oninput = function() {
+    outputGirasol.innerHTML = `Week: ${this.value}`;
+    document.getElementById("girasol").src=`assets/images/girasol/girasol${this.value}.jpg`;
+    
+        if(this.value == 1){
+            document.getElementById("girasolD").innerHTML = `Cambios Importantes:
+Color Verde Vivo: La albahaca presenta un color verde vivo que indica buena salud y vitalidad.
+Frondosidad: La planta se muestra frondosa, con un crecimiento denso y exuberante.
+Modificaciones Observadas:
+Crecimiento de Hojas: Se observan nuevas hojas en desarrollo.
+Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un indicativo de que la planta está absorbiendo bien los nutrientes y el agua.
+`;
+        }
+        if(this.value == 2){
+            document.getElementById("girasolD").innerHTML = "Observation: 2";
+        }
+        if(this.value == 3){
+            document.getElementById("girasolD").innerHTML = "Observation: 3";
+        }
+        if(this.value == 4){
+            document.getElementById("girasolD").innerHTML = "Observation: 4";
+        }
+        if(this.value == 5){
+            document.getElementById("girasolD").innerHTML = "Observation: 5";
+        }
+        if(this.value == 6){
+            document.getElementById("girasolD").innerHTML = "Observation: 6";
+        }
+        if(this.value == 7){
+            document.getElementById("girasolD").innerHTML = "Observation: 7";
+        }
+        if(this.value == 8){
+            document.getElementById("girasolD").innerHTML = "Observation: 8";
+        }
+        if(this.value == 9){
+            document.getElementById("girasolD").innerHTML = "Observation: 9";
+        }
+        if(this.value == 10){
+            document.getElementById("girasolD").innerHTML = "Observation: 10";
+        }
+    }
+
+//habas
+
+var sliderHabas = document.getElementById("myRangeHabas");
+var outputHabas = document.getElementById("demoHabas");
+outputHabas.innerHTML = sliderHabas.value;
+
+sliderHabas.oninput = function() {
+    outputHabas.innerHTML = `Week: ${this.value}`;
+    document.getElementById("habas").src=`assets/images/habas/habas${this.value}.jpg`;
+    
+        if(this.value == 1){
+            document.getElementById("habasD").innerHTML = `Cambios Importantes:
+Color Verde Vivo: La albahaca presenta un color verde vivo que indica buena salud y vitalidad.
+Frondosidad: La planta se muestra frondosa, con un crecimiento denso y exuberante.
+Modificaciones Observadas:
+Crecimiento de Hojas: Se observan nuevas hojas en desarrollo.
+Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un indicativo de que la planta está absorbiendo bien los nutrientes y el agua.
+`;
+        }
+        if(this.value == 2){
+            document.getElementById("habasD").innerHTML = "Observation: 2";
+        }
+        if(this.value == 3){
+            document.getElementById("habasD").innerHTML = "Observation: 3";
+        }
+        if(this.value == 4){
+            document.getElementById("habasD").innerHTML = "Observation: 4";
+        }
+        if(this.value == 5){
+            document.getElementById("habasD").innerHTML = "Observation: 5";
+        }
+        if(this.value == 6){
+            document.getElementById("habasD").innerHTML = "Observation: 6";
+        }
+        if(this.value == 7){
+            document.getElementById("habasD").innerHTML = "Observation: 7";
+        }
+        if(this.value == 8){
+            document.getElementById("habasD").innerHTML = "Observation: 8";
+        }
+        if(this.value == 9){
+            document.getElementById("habasD").innerHTML = "Observation: 9";
+        }
+        if(this.value == 10){
+            document.getElementById("habasD").innerHTML = "Observation: 10";
+        }
+    }
+
+//hinojo
+
+var sliderHinojo = document.getElementById("myRangeHinojo");
+var outputHinojo = document.getElementById("demoHinojo");
+outputHinojo.innerHTML = sliderHinojo.value;
+
+sliderHinojo.oninput = function() {
+    outputHinojo.innerHTML = `Week: ${this.value}`;
+    document.getElementById("hinojo").src=`assets/images/hinojo/hinojo${this.value}.jpg`;
+    
+        if(this.value == 1){
+            document.getElementById("hinojoD").innerHTML = `Cambios Importantes:
+Color Verde Vivo: La albahaca presenta un color verde vivo que indica buena salud y vitalidad.
+Frondosidad: La planta se muestra frondosa, con un crecimiento denso y exuberante.
+Modificaciones Observadas:
+Crecimiento de Hojas: Se observan nuevas hojas en desarrollo.
+Salud de la Planta: Las hojas se visualizan gruesas y robustas, lo cual es un indicativo de que la planta está absorbiendo bien los nutrientes y el agua.
+`;
+        }
+        if(this.value == 2){
+            document.getElementById("hinojoD").innerHTML = "Observation: 2";
+        }
+        if(this.value == 3){
+            document.getElementById("hinojoD").innerHTML = "Observation: 3";
+        }
+        if(this.value == 4){
+            document.getElementById("hinojoD").innerHTML = "Observation: 4";
+        }
+        if(this.value == 5){
+            document.getElementById("hinojoD").innerHTML = "Observation: 5";
+        }
+        if(this.value == 6){
+            document.getElementById("hinojoD").innerHTML = "Observation: 6";
+        }
+        if(this.value == 7){
+            document.getElementById("hinojoD").innerHTML = "Observation: 7";
+        }
+        if(this.value == 8){
+            document.getElementById("hinojoD").innerHTML = "Observation: 8";
+        }
+        if(this.value == 9){
+            document.getElementById("hinojoD").innerHTML = "Observation: 9";
+        }
+        if(this.value == 10){
+            document.getElementById("hinojoD").innerHTML = "Observation: 10";
+        }
+    }
 
 
 
